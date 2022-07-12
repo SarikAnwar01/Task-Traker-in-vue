8aaef8165e2d092caaf4c80a491c535d5ecf77f1
Sarik Anwar <sarikanwar007@gmail.com>
2022-07-12 13:55:56 +0545

    initial

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="onClick()" :style="{background:color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props:{
+        text: {
+            type: String,
+            default: 'Add Task'
+        },
+        color: {
+            type: String,
+            default: 'green'
+        }
+    },
+    methods:{
+        onClick(){
+           console.log('clicked')
+        }
+    }
+   
+}
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div  :key="task.id" v-for="task in tasks">
+  <Task @toggle-reminder="$emit('toggle-reminder',task.id)" @delete="$emit('delete',task.id)" :task="task"/>
+
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+    name: "Tasks ",
+    props:{
+        tasks: Array
+    },
+    components: {
+        Task
+    },
+    emits:['delete','toggle-reminder'],
+}
+</script>
